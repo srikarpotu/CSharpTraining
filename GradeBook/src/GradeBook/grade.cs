2438ab2536fdbb4eef1f2d3da5e31278cf5a7c2d
srikarpotu <75029986+srikarpotu@users.noreply.github.com>
2020-12-07 20:13:05 +0530

    programs

---
+++ GradeBook/src/GradeBook/grade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var marks=new[]{10,20,35.6,40.4,50};
+           var l=new List<double>() {10,20,30,40,50};
+           double r=0;
+           foreach(double n in l){
+               r+=n;
+           }
+           double a=r/l.Count;
+           Console.WriteLine(a);
+        }
+    }
+}
